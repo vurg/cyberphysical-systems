de5425aaad3914e3738cd20a3184d916e1af746f
Sam <samha@net.chalmers.se>
2024-03-27 20:02:18 +0100

    #3 - adds comments to helloworld.cpp

--- a5/helloworld.cpp
+++ a5/helloworld.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include "PrimeChecker.hpp"
+#include "PrimeChecker.hpp" // includes prime checker header file
 
 int main(int argc, char** argv) {
+    // checks for correct number of arguments
     if (argc == 2) {
         int number = std::stoi(argv[1]);
         PrimeChecker pc;
         // Replaced existing name with my own to reflect my current identity
         std::cout << "Vurgun, Nasit; " << number << " is a prime number? " << pc.isPrime(number) << std::endl;
     }
-    return 0;
+    return 0; // returns 0, indicating program completion
 }
