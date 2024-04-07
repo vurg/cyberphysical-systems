0bf7eac3d6e7e7a7ec99d10932120da0fca7ff94
Daniel Van Den Heuvel <heuvel@student.chalmers.se>
2024-04-07 14:15:35 +0000

    #8 Fail pipeline by returning isPrime false

--- a5/PrimeChecker.cpp
+++ a5/PrimeChecker.cpp
@@ -1,6 +1,7 @@
 #include "PrimeChecker.hpp"
 
 bool PrimeChecker::isPrime(uint16_t n) {
+    /*
     bool retVal{true};
     if (n<2 || 0 == n%2) {
         retVal = false;
@@ -14,4 +15,6 @@ bool PrimeChecker::isPrime(uint16_t n) {
         }
     }
     return retVal;
+    */
+    return false;
 }
